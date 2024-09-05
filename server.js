2f4640167eea7a78e8cce57c7ec5168a02b9be8a
VikramRai-Mumbai <vikramraimumbai@gmail.com>
2024-09-05 16:52:54 +0530

    changes done

--- server.js
+++ server.js
@@ -19,7 +19,12 @@ require("dotenv").config();
 app.use(require('express').json());
 app.use(require('express').urlencoded({ extended: false }));
 app.use(cors()); 
-
+app.use( function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  next();
+});
 // base route
 app.get("/", (req, res) => {
   res
